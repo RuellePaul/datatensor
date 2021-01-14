3316c3bf06177c15693d0e33a7bbd39c818bab4e
Paul Ruelle <paul.ruelle@widmee.com>
2021-01-14 20:18:47 +0100

    Cleaning

--- ui/src/layout/Main.tsx
+++ ui/src/layout/Main.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
-import {Breadcrumb, Layout, Menu} from 'antd';
-import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import {Layout, Menu} from 'antd';
+import {AppstoreTwoTone, CloudOutlined, CodeFilled, UserOutlined} from '@ant-design/icons';
 
 const {SubMenu} = Menu;
 const {Header, Content, Sider} = Layout;
@@ -13,60 +13,36 @@ const Main: FC<LayoutProps> = ({children}: LayoutProps) => {
 
     return (
         <main>
-            <Layout>
-                <Header className='header'>
-                    <div className='logo'/>
-                    <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
-                        <Menu.Item key='1'>App</Menu.Item>
-                        <Menu.Item key='2'>Profile</Menu.Item>
-                        <Menu.Item key='3'>About</Menu.Item>
+            <Header>
+
+            </Header>
+            <Layout style={{height: 'calc(100% - 64px)'}}>
+                <Sider width={220}>
+                    <Menu
+                        mode='inline'
+                        defaultSelectedKeys={['sub1']}
+                        style={{height: '100%', borderRight: 0}}
+                    >
+                        <Menu.Item key='sub1' icon={<AppstoreTwoTone/>}>Dashboard</Menu.Item>
+                        <SubMenu key='sub2' icon={<UserOutlined/>} title='Datasets'>
+                            <Menu.Item key='1'>Dogs & cat</Menu.Item>
+                            <Menu.Item key='2'>COCOA</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key='sub3' icon={<CloudOutlined/>}>Tasks</Menu.Item>
+                        <Menu.Item key='sub3' icon={<CodeFilled/>}>API</Menu.Item>
                     </Menu>
-                </Header>
-                <Layout>
-                    <Sider width={200} className='site-layout-background'>
-                        <Menu
-                            mode='inline'
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{height: '100%', borderRight: 0}}
-                        >
-                            <SubMenu key='sub1' icon={<UserOutlined/>} title='Datasets'>
-                                <Menu.Item key='1'>option1</Menu.Item>
-                                <Menu.Item key='2'>option2</Menu.Item>
-                                <Menu.Item key='3'>option3</Menu.Item>
-                                <Menu.Item key='4'>option4</Menu.Item>
-                            </SubMenu>
-                            <SubMenu key='sub2' icon={<LaptopOutlined/>} title='subnav 2'>
-                                <Menu.Item key='5'>option5</Menu.Item>
-                                <Menu.Item key='6'>option6</Menu.Item>
-                                <Menu.Item key='7'>option7</Menu.Item>
-                                <Menu.Item key='8'>option8</Menu.Item>
-                            </SubMenu>
-                            <SubMenu key='sub3' icon={<NotificationOutlined/>} title='subnav 3'>
-                                <Menu.Item key='9'>option9</Menu.Item>
-                                <Menu.Item key='10'>option10</Menu.Item>
-                                <Menu.Item key='11'>option11</Menu.Item>
-                                <Menu.Item key='12'>option12</Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
-                    <Layout style={{padding: '0 24px 24px'}}>
-                        <Breadcrumb style={{margin: '16px 0'}}>
-                            <Breadcrumb.Item>Home</Breadcrumb.Item>
-                            <Breadcrumb.Item>List</Breadcrumb.Item>
-                            <Breadcrumb.Item>App</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <Content
-                            className='site-layout-background'
-                            style={{
-                                padding: 24,
-                                margin: 0,
-                                minHeight: 280,
-                            }}
-                        >
-                            {children}
-                        </Content>
-                    </Layout>
+                </Sider>
+                <Layout style={{padding: '0 24px 24px'}}>
+                    <Content
+                        className='site-layout-background'
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                        }}
+                    >
+                        {children}
+                    </Content>
                 </Layout>
             </Layout>
         </main>
