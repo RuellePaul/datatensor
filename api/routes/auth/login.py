6454ddf26a452727cc2d278f7fd1bd52210108d0
Paul Ruelle <paul.ruelle@widmee.com>
2021-01-21 13:52:47 +0100

    wip refactor back auth 2

--- api/routes/auth/login.py
+++ api/routes/auth/login.py
@@ -1,11 +1,11 @@
-import requests
 import os
-from flask import Blueprint, request
+
+import requests
+from flask import Blueprint
 from oauthlib.oauth2 import WebApplicationClient
 from webargs import fields
 from webargs.flaskparser import use_args
 
-import errors
 from config import Config
 from logger import logger
 
@@ -23,28 +23,12 @@ def oauth_authorization(website):
     :return: authorization_url
     """
 
-    if website == 'google':
-        authorization_url = WebApplicationClient(Config.GOOGLE_CLIENT_ID).prepare_request_uri(
-            Config.GOOGLE_AUTHORIZATION_ENDPOINT,
-            redirect_uri=f'{Config.UI_URL}/oauthcallback/{website}',
-            scope=['openid', 'email', 'profile'],
-        )
-
-    elif website == 'github':
-        authorization_url = WebApplicationClient(Config.GITHUB_CLIENT_ID).prepare_request_uri(
-            Config.GITHUB_AUTHORIZATION_ENDPOINT,
-            redirect_uri=f'{Config.UI_URL}/oauthcallback/{website}',
-            scope=['openid', 'email', 'profile'],
-        )
-
-    elif website == 'stackoverflow':
-        authorization_url = WebApplicationClient(Config.STACKOVERFLOW_CLIENT_ID).prepare_request_uri(
-            Config.STACKOVERFLOW_AUTHORIZATION_ENDPOINT,
-            redirect_uri=f'{Config.UI_URL}/oauthcallback/{website}'
-        )
-
-    else:
-        raise errors.BadRequest('Wrong website provided')
+    client = WebApplicationClient(Config.OAUTH[website]['CLIENT_ID'])
+    authorization_url = client.prepare_request_uri(
+        Config.OAUTH[website]['AUTHORIZATION_URL'],
+        redirect_uri=f'{Config.UI_URL}/oauthcallback/{website}',
+        scope=Config.OAUTH[website]['SCOPES']
+    )
 
     logger.info(f'Fetch OAuth authorization url for {website}')
 
@@ -63,58 +47,27 @@ def oauth_callback(args):
     :return:
     """
 
-    if args['website'] == 'google':
-        client = WebApplicationClient(Config.GOOGLE_CLIENT_ID)
-        token_url, headers, body = client.prepare_token_request(
-            Config.GOOGLE_TOKEN_ENDPOINT,
-            redirect_url=f"{Config.UI_URL}/oauthcallback/{args['website']}",
-            code=args['code']
-        )
-        response = requests.post(
-            token_url,
-            headers=headers,
-            data=body,
-            auth=(Config.GOOGLE_CLIENT_ID, Config.GOOGLE_CLIENT_SECRET),
-        )
-        client.parse_request_body_response(response.text)
-        uri, headers, body = client.add_token(Config.GOOGLE_USER_ENDPOINT)
-        user_info = requests.get(uri, headers=headers, data=body).json()
-
-    if args['website'] == 'github':
-        client = WebApplicationClient(Config.GITHUB_CLIENT_ID)
-        token_url, headers, body = client.prepare_token_request(
-            Config.GITHUB_TOKEN_ENDPOINT,
-            redirect_url=f"{Config.UI_URL}/oauthcallback/{args['website']}",
-            code=args['code']
-        )
-        response = requests.post(
-            token_url,
-            headers=headers,
-            data=body,
-            auth=(Config.GITHUB_CLIENT_ID, Config.GITHUB_CLIENT_SECRET),
-        )
-        client.parse_request_body_response(response.text)
-        uri, headers, body = client.add_token(Config.GITHUB_USER_ENDPOINT)
-        user_info = requests.get(uri, headers=headers, data=body).json()
-
-    if args['website'] == 'stackoverflow':
-        client = WebApplicationClient(Config.STACKOVERFLOW_CLIENT_ID)
-        token_url, headers, body = client.prepare_token_request(
-            Config.STACKOVERFLOW_TOKEN_ENDPOINT,
-            redirect_url=f"{Config.UI_URL}/oauthcallback/{args['website']}",
-            code=args['code']
-        )
-        response = requests.post(
-            token_url,
-            headers=headers,
-            data=body + '&client_secret=' + Config.STACKOVERFLOW_CLIENT_SECRET
-        )
-        client.parse_request_body_response(response.text)
-        uri, headers, body = client.add_token(Config.STACKOVERFLOW_USER_ENDPOINT)
-        user_info = requests.get(uri, headers=headers, data=body, params={
-            'access_token': response.json()['access_token'],
-            'key': Config.STACKOVERFLOW_KEY
-        }).json()
+    website = args['website']
+    client = WebApplicationClient(Config.OAUTH[website]['CLIENT_ID'])
+    token_url, headers, body = client.prepare_token_request(
+        Config.OAUTH[website]['TOKEN_URL'],
+        redirect_url=f"{Config.UI_URL}/oauthcallback/{args['website']}",
+        code=args['code']
+    )
+    response = requests.post(
+        token_url,
+        headers=headers,
+        data=f'''{body}{f"&client_secret={Config.OAUTH[website]['CLIENT_SECRET']}"
+        if website == "stackoverflow" else ""}''',
+        auth=((Config.OAUTH[website]['CLIENT_ID'], Config.OAUTH[website]['CLIENT_SECRET'])
+              if website != 'stackoverflow' else None),
+    )
+    client.parse_request_body_response(response.text)
+    uri, headers, body = client.add_token(Config.OAUTH[website]['USER_URL'])
+    user_info = requests.get(uri, headers=headers, data=body, params={
+        'access_token': response.json()['access_token'],
+        'key': Config.OAUTH[website]['KEY']
+    } if args['website'] == 'stackoverflow' else {}).json()
 
     print(user_info)
 
