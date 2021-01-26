3c2d09e02cda86b31fb839e045401985d416d64b
Paul Ruelle <paul.ruelle@widmee.com>
2021-01-26 18:50:39 +0100

    wip design settings

--- ui/src/views/Settings.tsx
+++ ui/src/views/Settings.tsx
@@ -1,34 +1,84 @@
 import React, {FC} from 'react';
 import {api} from 'api';
-import {Form, Inputs, Structure} from 'components';
+import {Form, Inputs} from 'components';
 
-import {Box} from '@material-ui/core';
+import {Avatar, Box, Container, Divider, Typography} from '@material-ui/core';
+import {useUser} from 'hooks';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    wrapper: {
+        padding: theme.spacing(4, 0),
+        '& > h5, & > p': {
+            fontWeight: 600
+        }
+    },
+    avatar: {
+        width: 50,
+        height: 50
+    }
+}));
+
 
 const Settings: FC = () => {
 
+    const classes = useStyles();
+    const {user} = useUser();
+
     return (
-        <Structure.Center
-            title='Settings'
-        >
-            <Form
-                dynamic
-                schema={{
-                    name: {
-                        presence: {allowEmpty: false, message: 'Username is required'},
-                    }
-                }}
-                submit={formState => api.post('/settings/profile/update_name', formState!.values)
-                    .then(response => console.log(response.data))
-                }
-            >
-                <Box p='1rem'>
-                    <Inputs.Text
-                        name='name'
-                        label='Username'
-                    />
+        <Container maxWidth='md'>
+            <div className={classes.wrapper}>
+                <Avatar
+                    className={classes.avatar}
+                    src={user.avatar}
+                    alt='User avatar'
+                />
+
+                <Typography
+                    variant='h5'
+                    color='textPrimary'
+                    gutterBottom
+                >
+                    Profile
+                </Typography>
+
+                <Divider/>
+
+                <Box p='1rem 0'>
+                    <Form
+                        schema={{
+                            name: {
+                                presence: {allowEmpty: false, message: 'Username is required'},
+                            }
+                        }}
+                        submit={formState => api.post('/settings/profile/update_name', formState!.values)
+                            .then(response => console.log(response.data))
+                        }
+                        values={{
+                            name: user.name || ''
+                        }}
+                    >
+
+                        <Typography
+                            color='textPrimary'
+                        >
+                            Username
+                        </Typography>
+
+                        <Inputs.Text
+                            name='name'
+                            size='small'
+                        />
+                        <Typography
+                            variant='caption'
+                            color='textSecondary'
+                        >
+                            Your name may appear around Datatensor on your public datasets.
+                        </Typography>
+                    </Form>
                 </Box>
-            </Form>
-        </Structure.Center>
+            </div>
+        </Container>
     )
 };
 
