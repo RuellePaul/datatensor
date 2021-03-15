95a57473873b93ab91758b43b6a38b5251b81b9e
Paul Ruelle <paul.ruelle@widmee.com>
2021-03-16 00:28:06 +0100

    WIP deployment DTServerTest procedure

--- builds/test/DTServerTestLogin.sh
+++ builds/test/DTServerTestLogin.sh
@@ -3,4 +3,17 @@ ssh -i "DTServerTestKeys.pem" ec2-user@ec2-15-188-53-201.eu-west-3.compute.amazo
 # sudo -i
 # yum install git
 # yum install docker
+# service docker start
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+
+
 # git clone https://github.com/RuellePaul/datatensor.git
+# cd datatensor
+
+# cd builds/test
+# nano init_env.sh
+
+# cd ..
+# source deploy_service.sh
