ebe4391d61390c114a59e09f6a673f705576aa67
Paul Ruelle <paul.ruelle@widmee.com>
2021-03-16 01:34:29 +0100

    add docker login procedure

--- builds/test/DTServerTestLogin.sh
+++ builds/test/DTServerTestLogin.sh
@@ -8,6 +8,7 @@ ssh -i "DTServerTestKeys.pem" ec2-user@ec2-15-188-53-201.eu-west-3.compute.amazo
 # sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
 
+# cat ~/github_token.txt | docker login https://docker.pkg.github.com -u RuellePaul --password-stdin
 
 # git clone https://github.com/RuellePaul/datatensor.git
 # cd datatensor
