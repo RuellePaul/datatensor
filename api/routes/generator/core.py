6880aaaa76cc1a6b4e856f2476012a06dc167b42
Toms <roudilt@gmail.com>
2021-04-27 19:04:52 +0200

    WIP labels_from_annotations : need to code (breaking change) fetch images from annotations instead of images folder

--- api/routes/generator/core.py
+++ api/routes/generator/core.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 from datetime import datetime
 
 import cv2
+import json
 
 from flask import request
 
@@ -12,19 +13,28 @@ from routes.authentication.core import verify_access_token
 from routes.images.core import allowed_file, compress_image, upload_image, secure_filename
 
 
-def dataset_generation(name):
+def labels_from_annotations(annotations, filename):
+    annotations_path = os.path.join(Config.LOCAL_DATASETS_PATH, dataset_name, 'annotations', 'captions_train2014.json')
+    return []
+
+
+def dataset_generation(dataset_name):
     user = verify_access_token(request.headers['Authorization'], verified=True)
-    dataset_id = Config.LOCAL_DATASET_IDS[name]
+    dataset_id = Config.LOCAL_DATASET_IDS[dataset_name]
 
     if Config.db.datasets.find_one({'id': dataset_id}):
-        raise errors.Forbidden(f'Dataset {name} is already built')
+        raise errors.Forbidden(f'Dataset {dataset_name} is already built')
 
-    images_path = os.path.join(Config.LOCAL_DATASETS_PATH, name, 'images')
+    images_path = os.path.join(Config.LOCAL_DATASETS_PATH, dataset_name, 'images')
 
     try:
         image_filenames = os.listdir(images_path)
     except FileNotFoundError:
         raise errors.NotFound(f"{images_path} doesn't exists")
+
+    annotations_path = os.path.join(Config.LOCAL_DATASETS_PATH, dataset_name, 'annotations', 'instances_val2014.json')
+    json_file = open(annotations_path, 'r')
+    annotations = json.load(json_file)
 
     images = []
     for filename in image_filenames:
@@ -38,11 +48,11 @@ def dataset_generation(name):
                 'id': image_id,
                 'dataset_id': dataset_id,
                 'path': path,
-                'name': secure_filename(str(filename)),
+                'dataset_name': secure_filename(str(filename)),
                 'size': len(image_bytes),
                 'width': image.shape[1],
                 'height': image.shape[0],
-                'labels': []
+                'labels': labels_from_annotations(annotations, filename)
             })
 
     dataset = dict(id=dataset_id,
@@ -50,7 +60,7 @@ def dataset_generation(name):
                    created_at=datetime.now().isoformat(),
                    description='test',
                    is_public=True,
-                   name=name)
+                   dataset_name=dataset_name)
 
     Config.db.datasets.insert_one(dataset)
     Config.db.images.insert_many(images)
