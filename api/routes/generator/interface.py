686749a251652350227003411f32043ea6bd94c6
Paul Ruelle <paul.ruelle@widmee.com>
2021-04-29 18:25:11 +0200

    add dataset_name as arg for generaotr

--- api/routes/generator/interface.py
+++ api/routes/generator/interface.py
@@ -9,8 +9,9 @@ generator = Blueprint('generator', __name__)
 
 @generator.route('/coco', methods=['POST'])
 @use_args({
+    'dataset_name': fields.Str(required=True),
     'image_count': fields.Int(missing=10)
 })
 def init_dataset(args):
-    core.dataset_generation('coco', args['image_count'])
+    core.dataset_generation(args['dataset_name'], args['image_count'])
     return 'OK', 200
