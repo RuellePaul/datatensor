a1cd0c778a3a1ec8f73d909c93d1e251e2cddab8
Paul Ruelle <paul.ruelle@widmee.com>
2021-04-29 12:31:34 +0200

    enhance generator front end

--- ux/src/views/reports/AdminDashboardView/Generator/index.tsx
+++ ux/src/views/reports/AdminDashboardView/Generator/index.tsx
@@ -3,7 +3,7 @@ import {Formik} from 'formik';
 import * as Yup from 'yup';
 import {useSnackbar} from 'notistack';
 
-import {Box, Button, TextField} from '@material-ui/core';
+import {Box, Button, Paper, TextField} from '@material-ui/core';
 import {Alert} from '@material-ui/lab';
 
 import useIsMountedRef from 'src/hooks/useIsMountedRef';
@@ -16,87 +16,88 @@ const Generator: FC = () => {
     const {enqueueSnackbar} = useSnackbar();
 
     return (
-        <Formik
-            initialValues={{
-                image_count: 10
-            }}
-            validationSchema={Yup.object().shape({
-                image_count: Yup.number().max(1000).required(),
-            })}
-            onSubmit={async (values, {
-                setStatus,
-                setSubmitting
-            }) => {
-                try {
-                    await api.post('/v1/admin/generator/coco', values);
+        <Paper>
+            <Formik
+                initialValues={{
+                    image_count: 10
+                }}
+                validationSchema={Yup.object().shape({
+                    image_count: Yup.number().max(1000).required(),
+                })}
+                onSubmit={async (values, {
+                    setStatus,
+                    setSubmitting
+                }) => {
+                    try {
+                        await api.post('/v1/admin/generator/coco', values);
 
-                    if (isMountedRef.current) {
-                        setStatus({success: true});
-                        setSubmitting(false);
-                        enqueueSnackbar('Dataset COCO generated', {variant: 'info'});
+                        if (isMountedRef.current) {
+                            setStatus({success: true});
+                            setSubmitting(false);
+                            enqueueSnackbar(`Dataset generated (${values.image_count} images)`, {variant: 'info'});
+                        }
+                    } catch (error) {
+                        console.error(error);
+                        if (isMountedRef.current) {
+                            setStatus({success: false});
+                            setSubmitting(false);
+                            enqueueSnackbar(error.message, {variant: 'error'});
+                        }
                     }
-                } catch (error) {
-                    console.error(error);
-                    if (isMountedRef.current) {
-                        setStatus({success: false});
-                        setSubmitting(false);
-                        enqueueSnackbar(``, {variant: 'info'});
-                    }
-                }
-            }}
-        >
-            {({
-                  errors,
-                  handleBlur,
-                  handleChange,
-                  handleSubmit,
-                  isSubmitting,
-                  touched,
-                  values
-              }) => (
-                <form
-                    noValidate
-                    onSubmit={handleSubmit}
-                >
-                    <TextField
-                        error={Boolean(touched.image_count && errors.image_count)}
-                        fullWidth
-                        label="Image count"
-                        name="email"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        required
-                        type="email"
-                        value={values.image_count}
-                        variant="outlined"
-                        size="small"
-                    />
-                    <Box mt={2}>
-                        <Button
-                            color="secondary"
-                            disabled={isSubmitting}
+                }}
+            >
+                {({
+                      errors,
+                      handleBlur,
+                      handleChange,
+                      handleSubmit,
+                      isSubmitting,
+                      touched,
+                      values
+                  }) => (
+                    <form
+                        noValidate
+                        onSubmit={handleSubmit}
+                    >
+                        <TextField
+                            error={Boolean(touched.image_count && errors.image_count)}
                             fullWidth
-                            type="submit"
-                            variant="contained"
-                        >
-                            Generate dataset
-                        </Button>
-                    </Box>
-                    <Box mt={2}>
-                        <Alert
-                            severity="info"
-                        >
-                            <div>
-                                Use {' '} <b>carefully</b>.
-                                {' '}
-                                It uses a lot <b>AWS S3 storage</b>
-                            </div>
-                        </Alert>
-                    </Box>
+                            label="Image count"
+                            name="image_count"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            required
+                            value={values.image_count}
+                            variant="outlined"
+                            size="small"
+                        />
+                        <Box mt={2}>
+                            <Button
+                                color="secondary"
+                                disabled={isSubmitting}
+                                fullWidth
+                                type="submit"
+                                variant="contained"
+                            >
+                                Generate dataset
+                            </Button>
+                        </Box>
+                        <Box mt={2}>
+                            <Alert
+                                severity="info"
+                            >
+                                <div>
+                                    Use {' '} <b>carefully</b>.
+                                    {' '}
+                                    It uses a lot <b>AWS S3 storage</b>
+                                </div>
+                            </Alert>
+                        </Box>
 
-                </form>
-            )}
-        </Formik>
+                    </form>
+                )}
+            </Formik>
+        </Paper>
     )
 };
 
