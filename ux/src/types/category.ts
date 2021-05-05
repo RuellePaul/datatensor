6d3983cda1b115ddeff0f5b9d1b441369b699422
Paul Ruelle <paul.ruelle@widmee.com>
2021-05-05 21:42:59 +0200

    fix generator to include categories !!

--- ux/src/types/category.ts
+++ ux/src/types/category.ts
@@ -1,5 +1,5 @@
 export interface Category {
     id: string;
     name: string;
-    supercategory: 'person' | 'vehicle' | null;
+    supercategory: 'person' | 'vehicle' | 'electronic' | 'indoor' | 'outdoor' | 'sports' | 'furniture' | 'accessory' | 'kitchen' | 'animal' | 'appliance' | 'food' | null;
 }
