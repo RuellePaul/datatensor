273536cc7c3ea75d7107f53d9e0a2e384551332e
Paul Ruelle <paul.ruelle@widmee.com>
2021-05-13 22:58:54 +0200

    remove explore POC

--- ux/src/views/dataset/DatasetMainView/sections/SectionImages.tsx
+++ ux/src/views/dataset/DatasetMainView/sections/SectionImages.tsx
@@ -2,7 +2,6 @@ import React, {FC, useState} from 'react';
 import {useParams} from 'react-router';
 import {Box, Button, Dialog, DialogContent, DialogTitle, IconButton, makeStyles, Typography} from '@material-ui/core';
 import {Close as CloseIcon} from '@material-ui/icons';
-import DTExplore from 'src/components/datatensor/Explore';
 import DTImagesList from 'src/components/datatensor/ImagesList';
 import ImagesDropzone from 'src/components/ImagesDropzone';
 import useImages from 'src/hooks/useImages';
@@ -39,7 +38,6 @@ const SectionImage: FC = () => {
     const {images} = useImages();
 
     const [openUpload, setOpenUpload] = useState(false);
-    const [openExplore, setOpenExplore] = useState(false);
 
     const handleUploadOpen = () => {
         setOpenUpload(true);
@@ -47,14 +45,6 @@ const SectionImage: FC = () => {
 
     const handleCloseUpload = () => {
         setOpenUpload(false);
-    };
-
-    const handleExploreOpen = () => {
-        setOpenExplore(true);
-    };
-
-    const handleCloseExplore = () => {
-        setOpenExplore(false);
     };
 
     const imagesCount = images.length;
@@ -70,14 +60,9 @@ const SectionImage: FC = () => {
                 </Typography>
 
                 <Box display='flex'>
-                    <Button variant="outlined" color="primary" onClick={handleUploadOpen} size="small">
+                    <Button color="primary" onClick={handleUploadOpen} size="small">
                         Upload images
                     </Button>
-                    <Box ml={1}>
-                        <Button variant="contained" color="primary" onClick={handleExploreOpen} size="small">
-                            Explore images
-                        </Button>
-                    </Box>
                 </Box>
 
                 <Dialog
@@ -103,30 +88,6 @@ const SectionImage: FC = () => {
                             dataset_id={dataset_id}
                             callback={handleCloseUpload}
                         />
-                    </DialogContent>
-                </Dialog>
-
-                <Dialog
-                    open={openExplore}
-                    onClose={handleCloseExplore}
-                    maxWidth='lg'
-                >
-                    <DialogTitle disableTypography>
-                        <Typography variant='h4'>
-                            Explore Images
-                        </Typography>
-                        <IconButton
-                            className={classes.close}
-                            onClick={handleCloseUpload}
-                        >
-                            <CloseIcon/>
-                        </IconButton>
-                    </DialogTitle>
-                    <DialogContent>
-                        <Typography color='textSecondary' gutterBottom>
-                            Browse datatensor public datasets to find images
-                        </Typography>
-                        <DTExplore/>
                     </DialogContent>
                 </Dialog>
             </div>
