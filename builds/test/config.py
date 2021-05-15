78d2ec339a1e6f273b1b7bcf386a2b802ea26722
Paul Ruelle <paul.ruelle@widmee.com>
2021-05-15 18:33:23 +0200

    fix

--- builds/test/config.py
+++ builds/test/config.py
@@ -13,7 +13,7 @@ class Config:
     ENVIRONMENT = os.environ['ENVIRONMENT']
 
     ROOT_PATH = os.path.abspath(os.path.join(Flask(__name__).root_path, os.pardir))
-    DEFAULT_DATASETS_PATH = os.path.join(ROOT_PATH, 'api', 'generator', 'default_datasets')
+    DEFAULT_DATASETS_PATH = os.path.join(ROOT_PATH, 'api', 'manager', 'generator', 'default_datasets')
 
     UI_URL = 'https://test.datatensor.io'
     API_URI = 'https://test.datatensor.io/api'
