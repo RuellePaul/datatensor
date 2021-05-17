4ed7a3580e019e51d96a8f300bb0cd0f86d391f3
Paul Ruelle <paul.ruelle@widmee.com>
2021-05-17 11:25:09 +0200

    FIX 401 on first call /tasks. Split admin/non admin call

--- ux/src/views/reports/DashboardView/index.tsx
+++ ux/src/views/reports/DashboardView/index.tsx
@@ -1,7 +1,11 @@
 import React, {FC} from 'react';
-import {Container, makeStyles} from '@material-ui/core';
+import {Container, Grid, makeStyles} from '@material-ui/core';
 import {Theme} from 'src/theme';
 import Page from 'src/components/Page';
+import Header from '../AdminDashboardView/Header';
+import UsersOverTime from '../AdminDashboardView/UsersOverTime';
+import Generator from '../AdminDashboardView/Generator';
+import DTTasks from '../../../components/datatensor/Tasks';
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
