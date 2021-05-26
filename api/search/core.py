3cf73a7b29a7ec3a811c8a2978b980a8c3d3a4db
Toms <roudilt@gmail.com>
2021-05-26 21:29:15 +0200

    WIP search : add substring insensitive research

--- api/search/core.py
+++ api/search/core.py
@@ -5,22 +5,26 @@ db = Config.db
 
 def search_datasets_by_query(query):
     datasets = list(db.datasets.find(
-        {'$or': [{'user_id': query}, {'name': query}, {'description': query}]}))
+        {'$or': [{'user_id': {'$regex': query, '$options': 'i'}},
+                 {'name': {'$regex': query, '$options': 'i'}},
+                 {'description': {'$regex': query, '$options': 'i'}}]}))
     return datasets
 
 
 def search_images_by_query(query):
-    images = list(db.images.find({'name': query}))
+    images = list(db.images.find({'name': {'$regex': query, '$options': 'i'}}))
     return images
 
 
 def search_users_by_query(query):
     users = list(db.users.find(
-        {'$or': [{'email': query}, {'name': query}]}))
+        {'$or': [{'email': {'$regex': query, '$options': 'i'}},
+                 {'name': {'$regex': query, '$options': 'i'}}]}))
     return users
 
 
 def search_categories_by_query(query):
     categories = list(db.categories.find(
-        {'$or': [{'name': query}, {'supercategory': query}]}))
+        {'$or': [{'name': {'$regex': query, '$options': 'i'}},
+                 {'supercategory': {'$regex': query, '$options': 'i'}}]}))
     return categories
