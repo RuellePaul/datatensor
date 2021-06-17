933cd04401d9f3b3a79e6ddadcddfdcb168388ec
Paul Ruelle <paul.ruelle@widmee.com>
2021-06-17 19:30:32 +0200

    add test patch user

--- api/tests/test_users.py
+++ api/tests/test_users.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from fastapi.testclient import TestClient
 
 from app import app, PREFIX
-from routers.users.models import UsersResponse, UserResponse
+from routers.users.models import User, UsersResponse, UserResponse, UserUpdateProfileBody, UserUpdatePasswordBody
 from tests.conftest import Store
 
 client = TestClient(app)
@@ -56,6 +56,34 @@ class TestUsers:
                               headers={'Authorization': Store.admin_access_token})
         assert response.status_code == 200
         assert UserResponse.validate(response.json())
+
+    def test_valid_patch_user(self):
+        assert Store.user['name'] == 'Jack Sparrow'
+        assert Store.user['city'] is None
+        assert Store.user['country'] is None
+        assert Store.user['is_public'] is True
+        assert Store.user['phone'] is None
+
+        body = UserUpdateProfileBody(city='Bordeaux',
+                                     country='France',
+                                     is_public=False,
+                                     name='Paul',
+                                     phone='0603210047')
+        response = client.patch(f'{PREFIX}/users/me',
+                                headers={'Authorization': Store.access_token},
+                                json=body.dict())
+        assert response.status_code == 200
+
+        response = client.get(f'{PREFIX}/auth/me',
+                              headers={'Authorization': Store.access_token})
+        assert response.status_code == 200
+        assert User.validate(response.json())
+        Store.user = response.json()
+        assert Store.user['city'] == 'Bordeaux'
+        assert Store.user['country'] == 'France'
+        assert Store.user['is_public'] is False
+        assert Store.user['name'] == 'Paul'
+        assert Store.user['phone'] == '0603210047'
 
     def test_invalid_delete_user(self):
         response = client.delete(f'{PREFIX}/users/{Store.user["id"]}',
