ca08639ebacdf5a3d07b41031b3f033eef502a69
Ruelle Paul <47316542+RuellePaul@users.noreply.github.com>
2021-08-22 00:11:10 +0200

    Feature/change dataset privacy (#51)
    * WIP front end for changing dataset privacy
    
    * add backend (PATCH /datasets/<dataset_id>/privacy)
    
    Co-authored-by: Paul Ruelle <paul.ruelle@widmee.com>

--- ux/src/views/dataset/DatasetMainView/sections/SectionSettings.tsx
+++ ux/src/views/dataset/DatasetMainView/sections/SectionSettings.tsx
@@ -2,6 +2,7 @@ import React, {FC, useState} from 'react';
 import {
     Box,
     Button,
+    Chip,
     CircularProgress,
     Dialog,
     DialogActions,
@@ -20,7 +21,12 @@ import {SectionProps} from './SectionProps';
 import api from 'src/utils/api';
 import {Dataset} from 'src/types/dataset';
 import {useSnackbar} from 'notistack';
-import {Close as CloseIcon} from '@material-ui/icons';
+import {
+    Close as CloseIcon,
+    Lock as PrivateIcon,
+    Public as PublicIcon,
+    Visibility as PrivacyIcon
+} from '@material-ui/icons';
 import clsx from 'clsx';
 import DeleteIcon from '@material-ui/icons/DeleteOutline';
 import {useHistory} from 'react-router-dom';
@@ -28,6 +34,14 @@ import {useHistory} from 'react-router-dom';
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
         padding: theme.spacing(2, 0)
+    },
+    privacyAction: {
+        color: theme.palette.common.white,
+        backgroundColor: theme.palette.warning.main,
+        '&:hover': {
+            backgroundColor: theme.palette.warning.dark
+        }
+
     },
     deleteAction: {
         color: theme.palette.common.white,
@@ -45,6 +59,9 @@ const useStyles = makeStyles((theme: Theme) => ({
     loader: {
         width: '20px !important',
         height: '20px !important'
+    },
+    chip: {
+        marginLeft: 6
     }
 }));
 
@@ -56,7 +73,7 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
     const {enqueueSnackbar} = useSnackbar();
 
     const {saveDatasets} = useDatasets();
-    const {dataset, categories} = useDataset();
+    const {dataset, saveDataset, categories} = useDataset();
 
 
     const [openDeleteDataset, setOpenDeleteDataset] = useState(false);
@@ -67,14 +84,22 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
         setOpenDeleteDataset(false);
     };
 
+    const [openChangePrivacy, setOpenChangePrivacy] = useState(false);
+    const handleOpenChangePrivacy = () => {
+        setOpenChangePrivacy(true);
+    };
+    const handleCloseChangePrivacy = () => {
+        setOpenChangePrivacy(false);
+    };
+
     const [isDeleting, setIsDeleting] = useState(false);
     const handleDeleteDataset = async () => {
         setIsDeleting(true);
         try {
+            handleCloseDeleteDataset();
             await api.delete(`/datasets/${dataset.id}`);
             saveDatasets(datasets => datasets.filter((current: Dataset) => current.id !== dataset.id));
             enqueueSnackbar(`Deleted dataset ${dataset.name}`, {variant: 'info'});
-            handleCloseDeleteDataset();
             history.push('/app/manage/datasets')
         } catch (error) {
             enqueueSnackbar(error.message || 'Something went wrong', {variant: 'error'});
@@ -83,17 +108,48 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
         }
     }
 
+    const handleChangeDatasetPrivacy = async () => {
+        try {
+            handleCloseChangePrivacy();
+            await api.patch(`/datasets/${dataset.id}/privacy`, {is_public: !dataset.is_public});
+
+            saveDataset({
+                ...dataset,
+                is_public: !dataset.is_public
+            });
+        } catch (error) {
+            enqueueSnackbar(error.message || 'Something went wrong', {variant: 'error'});
+        }
+    }
+
     return (
         <div className={clsx(classes.root, className)}>
             <Grid container spacing={2}>
                 <Grid item sm={6} xs={12}>
+                    <Box mb={2}>
+                        <Alert severity="warning">
+                            <AlertTitle>Change privacy</AlertTitle>
+                            Be careful, a public dataset is visible by any user of Datatensor.
+                            <br/>
+                            This dataset is currently <strong>{dataset.is_public ? 'public' : 'private'}</strong>
+                        </Alert>
+                    </Box>
 
+                    <Button
+                        className={classes.privacyAction}
+                        onClick={handleOpenChangePrivacy}
+                        startIcon={<PrivacyIcon/>}
+                        variant="contained"
+                    >
+                        Change privacy
+                    </Button>
                 </Grid>
+
                 <Grid item sm={6} xs={12}>
                     <Box mb={2}>
                         <Alert severity="error">
-                            <AlertTitle>Dangerous section</AlertTitle>
-                            Be careful, actions here can lead to make breaking change <strong>forever</strong>
+                            <AlertTitle>Delete dataset</AlertTitle>
+                            Be careful, once deleted, there is no going back. <strong>It will be lost forever</strong>
                         </Alert>
                     </Box>
 
@@ -116,7 +172,6 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
                 </Grid>
             </Grid>
 
-
             <Dialog
                 disableRestoreFocus
                 fullWidth
@@ -128,7 +183,7 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
                     disableTypography
                 >
                     <Typography variant='h4'>
-                        Delete dataset | Confirmation
+                        Delete dataset
                     </Typography>
 
                     <IconButton
@@ -139,7 +194,7 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
                     </IconButton>
                 </DialogTitle>
                 <DialogContent>
-                    <Box my={2}>
+                    <Box my={1}>
                         <Typography color='textPrimary' gutterBottom>
                             Are you sure you want to delete dataset {dataset.name} ?
                         </Typography>
@@ -150,7 +205,9 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
                             This will delete
                             {' '}
                             <Typography component='span' style={{fontWeight: 'bold'}}>
-                                {dataset.image_count} images
+                                {dataset.image_count} image{dataset.image_count > 1 ? 's' : ''}
+                                {' '}
+                                (+ {dataset.augmented_count} augmented image{dataset.augmented_count > 1 ? 's' : ''})
                             </Typography>
                             , and
                             {' '}
@@ -179,6 +236,51 @@ const SectionSettings: FC<SectionProps> = ({className}) => {
                         </Button>
                     </Box>
                 </DialogActions>
+            </Dialog>
+
+            <Dialog
+                disableRestoreFocus
+                fullWidth
+                open={openChangePrivacy}
+                onClose={handleCloseChangePrivacy}
+            >
+                <DialogTitle
+                    className='flex'
+                    disableTypography
+                >
+                    <Typography variant='h4'>
+                        Change privacy
+                    </Typography>
+
+                    <IconButton
+                        className={classes.close}
+                        onClick={handleCloseChangePrivacy}
+                    >
+                        <CloseIcon/>
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent>
+                    <Box my={1}>
+                        <Typography color='textPrimary' gutterBottom>
+                            This dataset is currently :
+
+                            <Chip
+                                className={classes.chip}
+                                label={dataset.is_public ? 'Public' : 'Private'}
+                                icon={dataset.is_public ? <PublicIcon/> : <PrivateIcon/>}
+                                variant='outlined'
+                            />
+                        </Typography>
+
+                        <Button
+                            onClick={handleChangeDatasetPrivacy}
+                            variant="contained"
+                        >
+                            {dataset.is_public ? 'Make private' : 'Make public'}
+                        </Button>
+
+                    </Box>
+                </DialogContent>
             </Dialog>
         </div>
     )
