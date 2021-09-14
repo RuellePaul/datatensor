f7d65427183fb46dc2dcdd4582f095ec76e3219e
Paul Ruelle <paul.ruelle@widmee.com>
2021-09-15 00:06:05 +0200

    WIP Labelisator redesign 2

--- ux/src/components/core/Labelisator/index.tsx
+++ ux/src/components/core/Labelisator/index.tsx
@@ -1,7 +1,21 @@
 import React, {FC, forwardRef, useCallback, useEffect, useState} from 'react';
-import {AppBar, Button, Dialog, IconButton, makeStyles, Slide, Toolbar} from '@material-ui/core';
+import {Maximize as LabelIcon, Move as MoveIcon} from 'react-feather';
+import {
+    AppBar,
+    Button,
+    Dialog,
+    FormControlLabel,
+    IconButton,
+    makeStyles,
+    Slide,
+    Switch,
+    Toolbar,
+    Tooltip,
+    Typography
+} from '@material-ui/core';
+import {ToggleButton, ToggleButtonGroup} from '@material-ui/lab';
 import {TransitionProps} from '@material-ui/core/transitions';
-import {Close as CloseIcon} from '@material-ui/icons';
+import {Close as CloseIcon, Restore as RestoreIcon} from '@material-ui/icons';
 import DTImage from 'src/components/core/Images/Image';
 import ToolLabel from './ToolLabel';
 import ToolMove from './ToolMove';
@@ -9,7 +23,7 @@ import {Theme} from 'src/theme';
 import useDataset from 'src/hooks/useDataset';
 import {Image} from 'src/types/image';
 import api from 'src/utils/api';
-import {ImageProvider} from 'src/store/ImageContext';
+import {ImageConsumer, ImageProvider} from 'src/store/ImageContext';
 
 interface DTLabelisatorProps {
 }
@@ -21,6 +35,9 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
     toolbar: {
         justifyContent: 'space-between'
+    },
+    wrapper: {
+        position: 'relative'
     }
 }));
 
@@ -36,8 +53,14 @@ const DTLabelisator: FC<DTLabelisatorProps> = () => {
     const classes = useStyles();
 
     const {dataset} = useDataset();
+
     const [autoSwitch, setAutoSwitch] = useState<boolean>(true);
+
     const [tool, setTool] = useState<'label' | 'move'>('label');
+    const handleToolChange = (event: React.MouseEvent<HTMLElement>, newTool: 'label' | 'move' | null) => {
+        if (newTool !== null)
+            setTool(newTool);
+    };
 
     const index = window.location.hash.split('#')[1];
 
@@ -66,7 +89,7 @@ const DTLabelisator: FC<DTLabelisatorProps> = () => {
         window.location.hash = '';
     }
 
-    const [open, setOpen] = useState<boolean>(false);
+    const [open, setOpen] = useState<boolean>(window.location.hash.length > 0);
 
     useEffect(() => {
         const onHashChange = () => setOpen(window.location.hash.length > 0);
@@ -104,20 +127,124 @@ const DTLabelisator: FC<DTLabelisatorProps> = () => {
                     </Button>
                 </Toolbar>
             </AppBar>
-            {tool === 'label' && (
-                <ToolLabel
-                    setTool={setTool}
-                    autoSwitch={autoSwitch}
-                />
-            )}
-            {tool === 'move' && (
-                <ToolMove
-                    setTool={setTool}
-                    autoSwitch={autoSwitch}
-                />
-            )}
+
             <ImageProvider image={image}>
-                <DTImage skeleton/>
+                <div>
+                    <ToggleButtonGroup
+                        value={tool}
+                        exclusive
+                        onChange={handleToolChange}
+                        size='small'
+                    >
+                        <ToggleButton
+                            value="label"
+                            disabled={autoSwitch}
+                        >
+                            <Tooltip
+                                title={<Typography variant='overline'>
+                                    Draw tool (a)
+                                </Typography>}
+                            >
+                                <LabelIcon/>
+                            </Tooltip>
+                        </ToggleButton>
+                        <ToggleButton
+                            value="move"
+                            disabled={autoSwitch}
+                        >
+                            <Tooltip
+                                title={<Typography variant='overline'>
+                                    Move tool (z)
+                                </Typography>}
+                            >
+                                <MoveIcon/>
+                            </Tooltip>
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+
+                    <FormControlLabel
+                        control={(
+                            <Switch
+                                color="secondary"
+                                size='small'
+                                checked={autoSwitch}
+                                onChange={() => setAutoSwitch(!autoSwitch)}
+                            />
+                        )}
+                        label={(
+                            <Typography
+                                color='textSecondary'
+                            >
+                                Auto switch
+                            </Typography>
+                        )}
+                    />
+
+                    <div className='flexGrow'/>
+
+                    <ImageConsumer>
+                        {
+                            value => (
+                                <Tooltip
+                                    title={
+                                        <Typography variant='overline'>
+                                            Undo (CTRL + Z)
+                                        </Typography>
+                                    }
+                                >
+                                    <IconButton
+                                        disabled={value.positions.length <= 1}
+                                        onClick={value.previousPosition}
+                                    >
+                                        <RestoreIcon/>
+                                    </IconButton>
+                                </Tooltip>
+                            )
+                        }
+                    </ImageConsumer>
+
+                    <Tooltip
+                        title={
+                            <Typography variant='overline'>
+                                Save (SPACE)
+                            </Typography>
+                        }
+                    >
+                    <span>
+                        {<ImageConsumer>
+                            {
+                                value => (
+                                    <Button
+                                        variant="outlined"
+                                        color="secondary"
+                                        size='small'
+                                        onClick={value.validateLabels}
+                                    >
+                                        Save
+                                    </Button>
+                                )
+                            }
+                        </ImageConsumer>}
+                    </span>
+                    </Tooltip>
+                </div>
+
+                <div className={classes.wrapper}>
+                    {tool === 'label' && (
+                        <ToolLabel
+                            setTool={setTool}
+                            autoSwitch={autoSwitch}
+                        />
+                    )}
+                    {tool === 'move' && (
+                        <ToolMove
+                            setTool={setTool}
+                            autoSwitch={autoSwitch}
+                        />
+                    )}
+                    <DTImage skeleton/>
+
+                </div>
             </ImageProvider>
         </Dialog>
     );
