62ee3616ab114508d2b9ef4f2457447f5da1ce52
Paul Ruelle <paul.ruelle@widmee.com>
2021-09-22 17:25:42 +0200

    WIP SectionOverview : LabelisatorAction design

--- ux/src/views/dataset/DatasetMainView/sections/SectionOverview/LabelisatorAction.tsx
+++ ux/src/views/dataset/DatasetMainView/sections/SectionOverview/LabelisatorAction.tsx
@@ -1,8 +1,9 @@
 import React, {FC} from 'react';
-import {Button, makeStyles} from '@material-ui/core';
-import {Theme} from 'src/theme';
-import useImages from 'src/hooks/useImages';
 import clsx from 'clsx';
+import {Button, Card, CardActions, CardContent, makeStyles, Typography} from '@material-ui/core';
+import {Theme} from 'src/theme';
+import useDataset from 'src/hooks/useDataset';
+import useImages from 'src/hooks/useImages';
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {}
@@ -16,20 +17,49 @@ const LabelisatorAction: FC<LabelisatorActionProps> = ({className}) => {
 
     const classes = useStyles();
 
+    const {categories} = useDataset();
     const {images} = useImages();
+
+    const totalLabelsCount = categories
+        .map(category => category.labels_count || 0)
+        .reduce((acc, val) => acc + val, 0)
 
     if (images.length === 0)
         return null;
 
     return (
-        <div className={clsx(classes.root, className)}>
-            <Button
-                onClick={() => window.location.hash = images[0].id}
-                variant='outlined'
-            >
-                Labelisator
-            </Button>
-        </div>
+        <Card
+            className={clsx(classes.root, className)}
+            variant='outlined'
+        >
+            <CardContent>
+                <Typography
+                    color='textPrimary'
+                    gutterBottom
+                >
+                    This dataset currently contains
+                    {' '}
+                    <strong>
+                        {totalLabelsCount} labels.
+                    </strong>
+                </Typography>
+                <Typography
+                    color='textSecondary'
+                    gutterBottom
+                >
+                    {totalLabelsCount > 0 ? 'Edit labels' : 'Start labeling'} with our labelisator tool.
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button
+                    color='primary'
+                    onClick={() => window.location.hash = images[0].id}
+                    variant='outlined'
+                >
+                    Labelisator
+                </Button>
+            </CardActions>
+        </Card>
     )
 };
 
