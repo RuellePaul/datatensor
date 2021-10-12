4e32ba3c924e054770a3c98f24909062c698db3c
Paul Ruelle <paul.ruelle@widmee.com>
2021-10-12 17:42:15 +0200

    typo

--- ux/src/views/dataset/DatasetMainView/sections/SectionOverview/UploadAction.tsx
+++ ux/src/views/dataset/DatasetMainView/sections/SectionOverview/UploadAction.tsx
@@ -1,9 +1,11 @@
 import React, {FC} from 'react';
 import clsx from 'clsx';
-import {Card, CardActions, CardContent, CardHeader, Typography} from '@mui/material';
+import {Card, CardActions, CardContent, CardHeader, Link, Typography} from '@mui/material';
 import {makeStyles} from '@mui/styles';
 import {Theme} from 'src/theme';
 import UploadButton from 'src/components/core/Images/UploadButton';
+import {Link as RouterLink} from 'react-router-dom';
+
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {}
@@ -13,19 +15,23 @@ interface UploadActionProps {
     className?: string;
 }
 
-const UploadAction: FC<UploadActionProps> = ({className}) => {
+const UploadAction: FC<UploadActionProps> = ({ className }) => {
     const classes = useStyles();
 
     return (
         <Card className={clsx(classes.root, className)} variant="outlined">
             <CardHeader title="Upload" />
             <CardContent>
-                <Typography gutterBottom>Upload images</Typography>
+                <Typography gutterBottom>Add images</Typography>
                 <Typography variant="body2" color="textSecondary" gutterBottom>
-                    Lorem ipsum dolor
+                    A dataset should have enough images to train a computer vision AI model. See the{' '}
+                    <Link variant="body2" color="primary" component={RouterLink} to="/docs">
+                        dedicated section
+                    </Link>{' '}
+                    on documentation.
                 </Typography>
             </CardContent>
-            <CardActions style={{justifyContent: 'flex-end'}}>
+            <CardActions style={{ justifyContent: 'flex-end' }}>
                 <UploadButton />
             </CardActions>
         </Card>
