ad3e5f37c91fc91fae31c11dbd5804692898b02b
Paul Ruelle <paul.ruelle@widmee.com>
2021-10-18 17:53:45 +0200

    tweaks

--- ux/src/views/dataset/DatasetMainView/sections/SectionOverview/EditAction.tsx
+++ ux/src/views/dataset/DatasetMainView/sections/SectionOverview/EditAction.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import {useSnackbar} from 'notistack';
 import {Formik} from 'formik';
 import * as Yup from 'yup';
-import {Box, Button, CircularProgress, IconButton, Paper, TextField, Typography} from '@mui/material';
+import {Box, Button, CircularProgress, IconButton, Paper, TextField, Tooltip, Typography} from '@mui/material';
 import {CreateOutlined as EditIcon} from '@mui/icons-material';
 import {makeStyles} from '@mui/styles';
 import {Theme} from 'src/theme';
@@ -58,9 +58,11 @@ const EditAction: FC<EditProps> = ({ className }) => {
                         }}
                     />
                 </div>
-                <IconButton onClick={() => setEditing(true)}>
-                    <EditIcon />
-                </IconButton>
+                <Tooltip title="Edit">
+                    <IconButton onClick={() => setEditing(true)}>
+                        <EditIcon />
+                    </IconButton>
+                </Tooltip>
             </Box>
         );
     }
