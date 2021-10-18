8471ef1978d11c3ab76bc5b6ab0b5d7112d6f6ad
Paul Ruelle <paul.ruelle@widmee.com>
2021-10-18 14:02:35 +0200

    enhance scrollbar of DocsLayout (content)

--- ux/src/layouts/DocsLayout/index.tsx
+++ ux/src/layouts/DocsLayout/index.tsx
@@ -1,5 +1,6 @@
 import type {FC, ReactNode} from 'react';
 import React, {useState} from 'react';
+import PerfectScrollbar from 'react-perfect-scrollbar';
 import PropTypes from 'prop-types';
 import {MDXProvider} from '@mdx-js/react';
 import {Container} from '@mui/material';
@@ -45,16 +46,18 @@ const DocsLayout: FC<DocsLayoutProps> = ({ children }) => {
                 openMobile={isMobileNavOpen}
             />
             <div className={classes.wrapper}>
-                <div className={classes.contentContainer}>
-                    <Container
-                        maxWidth="md"
-                        className={classes.content}
-                    >
-                        <MDXProvider components={components}>
-                            {children}
-                        </MDXProvider>
-                    </Container>
-                </div>
+                <PerfectScrollbar options={{ suppressScrollX: true }}>
+                    <div className={classes.contentContainer}>
+                        <Container
+                            maxWidth="md"
+                            className={classes.content}
+                        >
+                            <MDXProvider components={components}>
+                                {children}
+                            </MDXProvider>
+                        </Container>
+                    </div>
+                </PerfectScrollbar>
             </div>
         </>
     );
