a5c1467852306d5cd3619f8fd273b05cf2c77e24
Paul Ruelle <paul.ruelle@widmee.com>
2021-10-20 20:54:04 +0200

    fixes

--- ux/src/views/dataset/DatasetMainView/sections/SectionImages/ImagesStackPanel.tsx
+++ ux/src/views/dataset/DatasetMainView/sections/SectionImages/ImagesStackPanel.tsx
@@ -1,15 +1,10 @@
 import React, {FC, useState} from 'react';
-import {Link as RouterLink} from 'react-router-dom';
-import {Box, Button, Card, CardContent, Grid, Link, Typography} from '@mui/material';
+import {Box, Button, Card, CardContent, Typography} from '@mui/material';
 import {makeStyles} from '@mui/styles';
 import {ArrowLeft as BackIcon} from 'react-feather';
 import DTImagesList from 'src/components/core/Images/ImagesList';
-import DTImagesStack from 'src/components/core/Images/ImagesStack';
-import UploadButton from 'src/components/core/Images/UploadButton';
 import useDataset from 'src/hooks/useDataset';
 import {Theme} from 'src/theme';
-import ViewPipelineAction from './ViewPipelineAction';
-import DeletePipelineAction from './DeletePipelineAction';
 import useCategory from 'src/hooks/useCategory';
 import useImages from 'src/hooks/useImages';
 import FilterCategories from './FilterCategories';
@@ -88,60 +83,12 @@ const ImagesStackPanel: FC<ImagesStackPanelProps> = ({pipeline = {}}) => {
     return (
         <Card className={classes.root} variant="outlined">
             <CardContent className={classes.content}>
-                {selected ? (
-                    currentCategory === null ? (
-                        <ImagesStackPanelExpanded pipeline_id={pipeline.id} setSelected={setSelected} />
-                    ) : (
-                        <ImagesProvider category_id={currentCategory.id} pipeline_id={pipeline.id}>
-                            <ImagesStackPanelExpanded setSelected={setSelected} pipeline_id={pipeline.id} />
-                        </ImagesProvider>
-                    )
+                {currentCategory === null ? (
+                    <ImagesStackPanelExpanded pipeline_id={pipeline.id} setSelected={setSelected} />
                 ) : (
-                    <Grid container spacing={1}>
-                        <Grid item md={5} xs={12} style={{justifyContent: 'center'}}>
-                            <DTImagesStack onClick={() => setSelected(true)} />
-                        </Grid>
-                        <Grid item md={7} xs={12}>
-                            {pipeline.id ? (
-                                <div className={classes.wrapper}>
-                                    <Typography variant="h4" color="textPrimary" gutterBottom>
-                                        Augmented image ({pipeline.image_count})
-                                    </Typography>
-
-                                    <ViewPipelineAction pipeline_id={pipeline.id} />
-                                    <DeletePipelineAction
-                                        pipeline_id={pipeline.id}
-                                        callback={() =>
-                                            savePipelines(pipelines =>
-                                                pipelines.filter(current => current.id !== pipeline.id)
-                                            )
-                                        }
-                                    />
-                                </div>
-                            ) : (
-                                <div className={classes.wrapper}>
-                                    <Typography variant="h4" color="textPrimary" gutterBottom>
-                                        Original images ({dataset.image_count})
-                                    </Typography>
-
-                                    <Typography color="textSecondary" gutterBottom>
-                                        These images needs to be labeled by hand, and will be used to perform
-                                        augmentation task to grow your dataset.
-                                    </Typography>
-
-                                    <Typography color="textSecondary" gutterBottom>
-                                        Check{' '}
-                                        <Link variant="subtitle1" color="primary" component={RouterLink} to="/docs">
-                                            original images
-                                        </Link>{' '}
-                                        section on our documentation to understand.
-                                    </Typography>
-
-                                    <UploadButton />
-                                </div>
-                            )}
-                        </Grid>
-                    </Grid>
+                    <ImagesProvider category_id={currentCategory.id} pipeline_id={pipeline.id}>
+                        <ImagesStackPanelExpanded setSelected={setSelected} pipeline_id={pipeline.id} />
+                    </ImagesProvider>
                 )}
             </CardContent>
         </Card>
