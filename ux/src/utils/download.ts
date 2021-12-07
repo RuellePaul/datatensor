31224109575df4f5b68719d4db5366c6609bae43
Paul Ruelle <paul.ruelle@widmee.com>
2021-12-07 15:28:43 +0100

    fix export bug

--- ux/src/utils/download.ts
+++ ux/src/utils/download.ts
@@ -1,6 +1,6 @@
 function download(content, fileName, contentType) {
     const a = document.createElement('a');
-    const file = new Blob([content], {type: contentType});
+    const file = new Blob([JSON.stringify(content, null, 4)], {type: contentType});
     a.href = URL.createObjectURL(file);
     a.download = fileName;
     a.click();
