8a80138cad7f0bb1c666c5f7c30a491402b07694
Paul Ruelle <paul.ruelle@widmee.com>
2021-12-12 18:09:16 +0100

    fix

--- ux/src/views/home/HomeView/index.tsx
+++ ux/src/views/home/HomeView/index.tsx
@@ -5,7 +5,6 @@ import Hero from './Hero';
 import Features from './Features';
 import Testimonials from './Testimonials';
 import CTA from './CTA';
-import FAQS from './FAQS';
 
 const useStyles = makeStyles(() => ({
     root: {}
