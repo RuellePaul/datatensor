addc44a1f306b519e60e795c55b3de3e72204490
Paul Ruelle <paul.ruelle@widmee.com>
2022-01-10 15:28:08 +0100

    WIP documentation content

--- ux/src/layouts/DocsLayout/mdx/Image.tsx
+++ ux/src/layouts/DocsLayout/mdx/Image.tsx
@@ -1,14 +1,16 @@
 import type {FC} from 'react';
 import React from 'react';
 import makeStyles from '@mui/styles/makeStyles';
+import {Theme} from 'src/theme';
 
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
     root: {
         display: 'block',
         margin: '24px auto',
         width: '100%',
-        maxWidth: 500
+        maxWidth: 700,
+        border: `solid 1px ${theme.palette.divider}`
     }
 }));
 
